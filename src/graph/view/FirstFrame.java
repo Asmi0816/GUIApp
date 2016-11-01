f7f6a193c3d0f0579c0c1b07b033bdc8916e3720
Arick Smith <kidwriter2020@gmail.com>
2016-11-01 12:37:05 -0600

    Built our panel so we could see things.

--- src/graph/view/FirstFrame.java
+++ src/graph/view/FirstFrame.java
@@ -4,21 +4,25 @@ import javax.swing.JFrame;
 import graph.controller.GraphController;
 import java.awt.Dimension;
 
+
 public class FirstFrame extends JFrame
 {
 	private GraphController baseController;
+	private FirstPanel appPanel;
 	
 	public FirstFrame(GraphController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		
+		this.appPanel = new FirstPanel(baseController);
 		
 		this.setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
+		this.setTitle("gee wilikers");
 		this.setLocationRelativeTo(null);
 		this.setSize(new Dimension(600, 400));
 		this.setVisible(true);
---
+++ src/graph/view/FirstPanel.java
@@ -0,0 +1,36 @@
+package graph.view;
+
+
+import javax.swing.JPanel;
+import graph.controller.GraphController;
+
+
+public class FirstPanel extends JPanel
+{
+	private GraphController baseController;
+	
+	public FirstPanel(GraphController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}

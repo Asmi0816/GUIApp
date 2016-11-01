3f34cb56a2bb1e6a0d164da4fdcdc051ca70f74b
Arick Smith <kidwriter2020@gmail.com>
2016-11-01 12:16:05 -0600

    Made a window pop up in the project.

--- src/graph/view/FirstFrame.java
+++ src/graph/view/FirstFrame.java
@@ -2,6 +2,7 @@ package graph.view;
 
 import javax.swing.JFrame;
 import graph.controller.GraphController;
+import java.awt.Dimension;
 
 public class FirstFrame extends JFrame
 {
@@ -12,5 +13,14 @@ public class FirstFrame extends JFrame
 		super();
 		this.baseController = baseController;
 		
+		
+		this.setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setLocationRelativeTo(null);
+		this.setSize(new Dimension(600, 400));
+		this.setVisible(true);
 	}
 }

db87b16ac4c7af37f7c3d7ad9aef2f7b0ff96755
Arick Smith <kidwriter2020@gmail.com>
2016-11-01 13:19:06 -0600

    Made the clicking of the button do something.

--- src/graph/view/FirstPanel.java
+++ src/graph/view/FirstPanel.java
@@ -1,23 +1,27 @@
 package graph.view;
 
 
-import javax.swing.JPanel;
-import graph.controller.GraphController;
+import javax.swing.*;
 import java.awt.Color;
-import javax.swing.JButton;
-
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import graph.controller.GraphController;
 
 public class FirstPanel extends JPanel
 {
 	private GraphController baseController;
 	private JButton colorButton;
 	private JButton randomButton;
+	private SpringLayout baseLayout;
+	
 	
 	public FirstPanel(GraphController baseController)
 	{
 		super();
 		this.baseController = baseController;
 		this.colorButton = new JButton("colors");
+		this.baseLayout = new SpringLayout();
 		this.randomButton = new JButton("wha!?!? colors XD");
 		setupPanel();
 		setupLayout();
@@ -26,6 +30,7 @@ public class FirstPanel extends JPanel
 	
 	private void setupPanel()
 	{
+		this.setLayout(baseLayout);
 		this.setBackground(Color.GREEN);
 		this.add(colorButton);
 		this.add(randomButton);
@@ -33,11 +38,27 @@ public class FirstPanel extends JPanel
 	
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.WEST, colorButton, 183, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, colorButton, -185, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, randomButton, 20, SpringLayout.SOUTH, colorButton);
+		baseLayout.putConstraint(SpringLayout.WEST, randomButton, 149, SpringLayout.WEST, this);
+		colorButton.setForeground(Color.RED);
+		colorButton.setFont(new Font("Century Schoolbook", Font.PLAIN, 13));
 	}
 	
 	private void setupListeners()
 	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				
+			}
+		});
+		
 		
 	}
+	
+	
+	
 }
